968e4b645aacb645f4fdb62c7e50b9d76917a077
meldahl <karlbendik2004@gmail.com>
2025-02-28 21:59:31 +0100

    Initial Commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn random(min: i8, max: i8) -> i8 {
+    return rand::thread_rng().gen_range(min..=max);
+}
+
+fn main() {
+    println!("Guess the number!");
+
+    let random_number = random(1, 100);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("\nPlease input a number!\n");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
